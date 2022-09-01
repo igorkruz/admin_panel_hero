ea04bd4d31f9187438a38fd89cd6bb7ecfe81087
igor <ikruzhynskyy@gmail.com>
2022-09-01 13:29:31 +0300

    rewrote the code using enchance/ middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,43 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 
 import filters from '../reducers/filters';
 import heroes from '../reducers/heroes';
+                // ({dispatch, getState})
+const customMiddleware = () => (next) => (action) => {
+                                // (dispatch)
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        })
+    }
+    return next(action)
+}
 
-const store = createStore(combineReducers({heroes, filters}),
-                             window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const enhancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+
+    const oldDispatch = store.dispatch;
+
+    store.dispatch = (actions) => {
+        if (typeof actions === 'string') {
+            return oldDispatch({
+                type: actions
+            })
+        }
+        return oldDispatch(actions)
+    }
+    return store;
+}
+
+const store = createStore(
+    combineReducers({ heroes, filters }),
+    compose(applyMiddleware(customMiddleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+);
+// compose(
+//     enhancer,
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// ));
 
 export default store;
+
