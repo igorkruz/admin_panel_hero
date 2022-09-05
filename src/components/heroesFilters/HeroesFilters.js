a2317992a23e2fd93c835f0e523d2f119ace88b5
igor <ikruzhynskyy@gmail.com>
2022-09-05 13:32:31 +0300

    use createAsyncThunk redux/toolkit

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -1,22 +1,19 @@
-import {useHttp} from '../../hooks/http.hook';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import classNames from 'classnames';
 
-import { fetchFilters, } from '../../actions';
-import{activeFilterChanged} from './filtersSlice'
+import{activeFilterChanged, fetchFilters} from './filtersSlice'
 import Spinner from '../spinner/Spinner';
 
 const HeroesFilters = () => {
 
     const { filters, filtersLoadingStatus, activeFilter } = useSelector(state => state.filters);
     const dispatch = useDispatch();
-    const { request } = useHttp();
 
     // запит на сервер для отримання фільтрів та покрокової зміни стану
 
     useEffect(() => {
-        dispatch(fetchFilters(request))
+        dispatch(fetchFilters())
         
     // eslint-disable-next-line
     }, [])
